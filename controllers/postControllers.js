abd8941a5fde14675cc9b041920a1fbbe6a6c1e5
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-06 16:21:54 +0100

    add update route

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -13,6 +13,38 @@ const store = (req,res) =>{
     })
 }
 
+const update = (req,res) => {
+    const post = myPost.find(post => post.title.toLocaleLowerCase() === req.params.title)
+
+    if(!post){
+        res.status(404).json({
+            error : 'no post found with that title'
+        })
+    }
+    post.title = req.body.title
+    post.slug = req.body.slug
+    post.content = req.body.content
+    post.image = req.body.image
+    post.tags = [req.body.tags]
+
+    fs.writeFileSync('./database/db.js', `module.exports = ${JSON.stringify(myPost, null, 4)}`)
+
+    res.status(200).json({
+        status : 200,
+        data : post
+    })
+}
+
+
+
+
+
+
+
+
+
+
 module.exports = {
-    store
+    store,
+    update
 }
--- routes/post.js
+++ routes/post.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsController = require('../controllers/postControllers.js');
 
 router.post('/', postsController.store);
+router.put('/:id', postsController.update )
 
 module.exports = router
