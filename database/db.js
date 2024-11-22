502d10e1b9c3d582bcdd8b7eec3f1881a1b4b8cc
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-22 15:39:47 +0100

    edit status

--- database/db.js
+++ database/db.js
@@ -1,30 +1,20 @@
 module.exports = [
     {
-        "title": "Pasta barbabietola e gorgonzola",
-        "slug": "pasta-barbabietola-e-gorgonzola",
-        "content": "La nostra ricetta della pasta barbabietola e gorgonzola vuole ricreare in questo primo piatto un abbinamento appetitoso, già proposto con la torta salata alla barbabietola! Per un pranzo veloce ma gustoso, per chi ama giocare con consistenze e colori naturali in cucina, questa pasta è perfetta! La dolcezza della barbabietola smorza il gusto deciso che caratterizza questo formaggio erborinato molto amato, un'abbinata vincente e molto gustosa. Provate un nuovo condimento per la vostra pasta e sperimentate altre sfiziose varianti:\n            Pasta con barbabietola e pecorino\n            Gnocchi di barbabietola\n            Tagliatelle alla barbabietola con asparagi",
-        "image": "https://ricette.giallozafferano.it/images/ricette/267/26704/pasta-con-crema-di-barbabietola-e-gorgonzola_21.jpg",
-        "tags": [
-            "Primi piatti",
-            "Ricette vegetariane"
-        ]
-    },
-    {
+        "status": "Published",
         "title": "PASTA",
         "slug": "pasta-barbabietola-e-gorgonzola",
-        "content": "La nostra ricetta della pasta barbabietola e gorgonzola vuole ricreare in questo primo piatto un abbinamento appetitoso, già proposto con la torta salata alla barbabietola! Per un pranzo veloce ma gustoso, per chi ama giocare con consistenze e colori naturali in cucina, questa pasta è perfetta! La dolcezza della barbabietola smorza il gusto deciso che caratterizza questo formaggio erborinato molto amato, un'abbinata vincente e molto gustosa. Provate un nuovo condimento per la vostra pasta e sperimentate altre sfiziose varianti:\n            Pasta con barbabietola e pecorino\n            Gnocchi di barbabietola\n            Tagliatelle alla barbabietola con asparagi",
+        "content": "La nostra ricetta della pasta barbabietola e gorgonzola vuole ricreare in questo primo piatto un abbinamento appetitoso, già proposto con la torta salata alla barbabietola! Per un pranzo veloce ma gustoso, per chi ama giocare con consistenze e colori naturali in cucina, questa pasta è perfetta! La dolcezza della barbabietola smorza il gusto deciso che caratterizza questo formaggio erborinato molto amato, un'abbinata vincente e molto gustosa. Provate un nuovo condimento per la vostra pasta e sperimentate altre sfiziose varianti:\n            Pasta con barbabietola e pecorino\n            Gnocchi di barbabietola\n            Tagliatelle alla barbabietola con asparagi.",
         "image": "https://ricette.giallozafferano.it/images/ricette/267/26704/pasta-con-crema-di-barbabietola-e-gorgonzola_21.jpg",
         "tags": [
-            "Primi piatti",
-            "Ricette vegetariane"
+            "Primi piatti"
         ]
     },
     {
-        "title": "dada",
-        "image": "/img/0.jpg",
-        "content": "dadada",
-        "tags": [],
-        "slug": "dada",
-        "status": "Published"
+        "title": "Pacman",
+        "status": "Draft",
+        "image": "blob:http://localhost:5173/8ededdf9-c750-4e2e-9603-f5d13617504e",
+        "content": "Ha spopolato durante gli anni Ottanta. Ha avuto innumerevoli tentativi di imitazione. E’ diventato un’icona, rappresentante dell’intero settore dell’industria dei videogiochi. Partorito dalla mente del programmatore giapponese Tohru Iwatani, allora dipendente della Namco, Pac-Man è un videogioco che ha fatto la storia dei videogiochi.\n\nL’origine di Pac-Man è molto curiosa: Tohru Iwatani ebbe infatti l’ispirazione durante una cena con degli amici guardando una pizza a cui era stata tolta una fetta. Dopo poco più di un anno da quella pizza, un team di sviluppo di otto persone tra sviluppatori e tecnici hardware diede alla luce il primo Pac-Man. Pubblicato dalla statunitense Midway Games, Pac Man compare per la prima volta nel formato arcade da sala il giorno 3 aprile 1980. La diffusione e insieme la popolarità in pochi anni raggiungono livelli altissimi; Namco si dedica alla produzione del software in varie versioni, per la quasi totalità delle console e dei computer. Dal 1980 al 1987 l’azienda Namco realizza più di 300 mila macchine, vendendo nel contempo un numero imprecisato di milioni di pupazzi e gadget vari.",
+        "tags": "React",
+        "slug": "pacman"
     }
 ]
